4158a9b68adbfc32aaf4d8d742823c07d5055919
jaime espinoza <jaimsepnz1@gmail.com>
2018-04-30 16:13:46 -0500

    Jaime Espinoza commits Storage Manager View Package and SMView Class

---
+++ src/storageManagerView/SMView.java
@@ -0,0 +1,25 @@
+package storageManagerView;
+
+
+import javax.swing.*;
+import java.awt.event.ActionListener;;
+
+public class SMView extends JFrame{
+
+    private JTextField tfUserNm = new JTextField(20);
+    private JTextField tfPassWd = new JTextField(20);
+
+    SMView(){
+
+        JPanel finalPanel = new JPanel();
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 200);
+
+        finalPanel.add(tfUserNm);
+        finalPanel.add(tfPassWd);
+
+    }
+
+
+}
