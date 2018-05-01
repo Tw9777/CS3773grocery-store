d100954e728effbe3d684dedff207a3cd04cbcaa
Jonathan Solis <solisj32@yahoo.com>
2018-04-30 22:08:24 -0500

    Jonathan Solis commits changes to SMView and StorageManagerPortal.

--- src/main/StorageManagerPortal.java
+++ src/main/StorageManagerPortal.java
@@ -1,15 +1,19 @@
 package main;
 
 import storageManagerModel.SMModel;
+import storageManagerView.SMView;
 
 public class StorageManagerPortal {
 
 	public static void main(String[] args) {
 		
 		SMModel m = new SMModel();
+		SMView s = new SMView();
 
-		if (m.logIn("User", "12345"))
+		if (m.logIn("User", "12345")) {
 			System.out.println("Login Successful!!");
+			s.setVisible(true);
+		}
 		else
 			System.out.println("Login Failed");
 
--- src/storageManagerView/SMView.java
+++ src/storageManagerView/SMView.java
@@ -2,25 +2,52 @@ package storageManagerView;
 
 
 import javax.swing.*;
-import java.awt.event.ActionListener;;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 
 public class SMView extends JFrame{
 
     private JTextField tfUserNm = new JTextField(20);
     private JTextField tfPassWd = new JTextField(20);
+    private JLabel lUserNm = new JLabel("Username");
+    private JLabel lPassWd = new JLabel("Password");
+    private JButton bLoginButton = new JButton("Enter");
+    
 
     // CONSTRUCTER
-    SMView(){
+    public SMView(){
 
-        JPanel finalPanel = new JPanel();
+    	this.setTitle("Storage Manager Portal");
+        JPanel SMPanel = new JPanel();
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(600, 200);
+        this.setSize(700, 200);
+        
+        SMPanel.setLayout(new GridLayout(5,1));
 
-        finalPanel.add(tfUserNm);
-        finalPanel.add(tfPassWd);
+        SMPanel.add(lUserNm);
+        SMPanel.add(tfUserNm);
+        SMPanel.add(lPassWd);
+        SMPanel.add(tfPassWd);
+        SMPanel.add(bLoginButton);
+        
+        this.add(SMPanel);
 
     }
-
+    
+    public String getUserNm() {
+    	
+    	return tfUserNm.getText();
+    }
+    
+    public String getPassWd() {
+    	
+    	return tfPassWd.getText();
+    }
+    
+    public void addLoginListener(ActionListener listenForLogin) {
+    	
+    	bLoginButton.addActionListener(listenForLogin);
+    }
 
 }
