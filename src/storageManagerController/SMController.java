88972b23d3662959dc7f18995785c8a30ad12669
James Solis <jamessolis2034@gmail.com>
2018-04-30 19:55:04 -0500

    James Solis commits the Storage Manager Controller package and SMController class

---
+++ src/storageManagerController/SMController.java
@@ -0,0 +1,23 @@
+package storageManagerController;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import storageManagerModel.SMModel;
+import storageManagerView.SMView;
+
+
+public class SMController {
+	
+	private SMView theView;
+	private SMModel theModel;
+	
+	public SMController (SMView theView, SMModel theModel){
+		this.theView = theView;
+		this.theModel = theModel;
+		
+		
+	
+	}
+
+}
